ee94b294c53cf07d3d2ad2eb10a6da4ebcf2eec7
mustafatok2016 <mustafa.tk33@gmail.com>
2020-08-16 17:45:11 +0000

    added codes

---
+++ R1Soft/null.tf
@@ -0,0 +1,31 @@
+resource "null_resource"  "commands" {
+    depends_on = ["aws_instance.web"]
+    triggers = {
+        always_run = "${timestamp()}"
+    }
+    # Push files  to remote server
+    provisioner "file" {
+        connection {
+        host = "${aws_instance.web.public_ip}"
+        type = "ssh"
+        user = "centos"
+        private_key = "${file("~/.ssh/id_rsa")}"
+        }
+        source      = "r1soft.repo"
+        destination = "/tmp/r1soft.repo"
+    }
+    # Execute linux commands on remote machine
+    provisioner "remote-exec" {
+        connection {
+        host = "${aws_instance.web.public_ip}"
+        type = "ssh"
+        user = "centos"
+        private_key = "${file("~/.ssh/id_rsa")}"
+    }
+    inline = [
+        "sudo yum install -y epel-release -y",
+        "sudo cp  /tmp/r1soft.repo  /etc/yum.repos.d/r1soft.repo",
+        "sudo yum install serverbackup-enterprise -y",
+        ]
+    }
+}
